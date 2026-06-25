c5bfd3cb6665463776171d54c737f92e9ee62db3
ZAIDI-MD-BOTZ <ideapaid911@gmail.com>
2026-06-25 21:49:04 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
     // 1. CONFIGURATION DE BASE (Session & Database)
     // ===========================================================
     SESSION_ID: process.env.SESSION_ID || "MINI BOT", 
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://offarslan_db_user:arslanmd@cluster0.xrqkzwg.mongodb.net/?appName=Cluster0',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://z7661877_db_user:mzDdJP3WDPCCoUxd@cluster0.ffcjq7q.mongodb.net/',
     
     // ===========================================================
     // 2. INFORMATIONS DU BOT
